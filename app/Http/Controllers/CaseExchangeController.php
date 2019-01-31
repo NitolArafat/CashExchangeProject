72d0b658dd89079c02193e0082ae4e554bc3e32b
NitolArafat <mdaaanitol@gmail.com>
2019-01-31 19:54:36 +0600

    first commit

---
+++ app/Http/Controllers/CaseExchangeController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CaseExchangeController extends Controller
+{
+   public function index(){
+       return '<h1>This is home page</h1>';
+   }
+}
